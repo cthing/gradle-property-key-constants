d9080b53dc4bb07a92d26d4d04c41fab476a2a90
Baron Roberts <baron@cthing.com>
2025-02-02 11:36:31 -0800

    Migrate to gradle-cthing-publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ plugins {
     checkstyle
     jacoco
     signing
+    alias(libs.plugins.cthingPublishing)
     alias(libs.plugins.cthingVersioning)
     alias(libs.plugins.dependencyAnalysis)
     alias(libs.plugins.pluginPublish)
@@ -146,7 +147,7 @@ tasks {
 
     publishPlugins {
         doFirst {
-            if (!project.hasProperty("gradle.publish.key") || !project.hasProperty("gradle.publish.secret")) {
+            if (!cthingPublishing.hasGradlePluginPortalCredentials()) {
                 throw GradleException("Gradle Plugin Portal credentials not defined")
             }
         }
@@ -175,7 +176,7 @@ tasks {
 
     withType<Sign>().configureEach {
         onlyIf("Signing credentials are present") {
-            hasProperty("signing.keyId") && hasProperty("signing.password") && hasProperty("signing.secretKeyRingFile")
+            cthingPublishing.canSign()
         }
     }
 
@@ -192,16 +193,21 @@ tasks {
 }
 
 publishing {
-    val repoUrl = if ((version as ProjectVersion).isSnapshotBuild)
-        findProperty("cthing.nexus.snapshotsUrl") else findProperty("cthing.nexus.candidatesUrl")
+    afterEvaluate {
+        publications.withType<MavenPublication> {
+            pom(cthingPublishing.createPomAction())
+        }
+    }
+
+    val repoUrl = cthingRepo.repoUrl
     if (repoUrl != null) {
         repositories {
             maven {
                 name = "CThingMaven"
                 setUrl(repoUrl)
                 credentials {
-                    username = property("cthing.nexus.user") as String
-                    password = property("cthing.nexus.password") as String
+                    username = cthingRepo.user
+                    password = cthingRepo.password
                 }
             }
         }
