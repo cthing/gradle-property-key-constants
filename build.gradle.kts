cbcc53929f993f6ed91f1b4352b1fe19e1f4a1d9
Baron Roberts <baron@cthing.com>
2025-10-04 18:15:29 -0700

    Generate release candidate.

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     alias(libs.plugins.versions)
 }
 
-version = ProjectVersion("2.0.1", BuildType.snapshot)
+version = ProjectVersion("2.1.0", BuildType.release)
 group = "org.cthing"
 description = "A Gradle plugin that generates a Java source file with constants for keys in properties files."
 
